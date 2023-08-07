791df18f99a029d75eb33609e3dbfbf1bdb55209
nheingit <NHeinDev@Gmail.com>
2023-08-07 11:12:30 -0500

    deleted redis config, changed sidekiq config to handle prod

--- config/initializers/redis.rb
+++
@@ -1,4 +0,0 @@
-# config/initializers/redis.rb
-uri = URI.parse(ENV["REDIS_URL"] || "redis://localhost:6379/")
-$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,22 @@
 require 'sidekiq-status'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  if Rails.env.production?
+    config.redis = { url: ENV['REDIS_URL'] }
+  else
+    config.redis = { url: 'redis://localhost:6379/0' }
+  end
   config.server_middleware do |chain|
     chain.add Sidekiq::Status::ServerMiddleware
   end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  if Rails.env.production?
+    config.redis = { url: ENV['REDIS_URL'] }
+  else
+    config.redis = { url: 'redis://localhost:6379/0' }
+  end
   config.client_middleware do |chain|
     chain.add Sidekiq::Status::ClientMiddleware
   end
